9e5ed4c12d14590bda36906189b12f6d7c758b6e
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-02-29 22:44:40 +0100

    Initial commit

---
+++ src/main/java/com/markowski/countriesdocker/CountriesDockerApplication.java
@@ -0,0 +1,13 @@
+package com.markowski.countriesdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/markowski/countriesdocker/CountriesDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markowski.countriesdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
